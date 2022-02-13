a2474f67770b553cca24c630318f8e0f2219f0cf
kise <kaiazmi1412@gmail.com>
2022-02-13 22:32:33 +0530

    2d array completed

--- JS/arrays.js
+++ JS/arrays.js
@@ -41,12 +41,19 @@ console.log(cars);
 console.log(cars.length);
 
 // 2d array
+// ["a", "b", "c"] // array of strings
+// [1, 2, 3] // array of numbers
 
 let array2d = [
 	[1, 2, 3],
 	[4, 5, 6],
 	[7, 8, 9],
-];
-console.log(array2d);
-console.table(array2d);
+]; // array of arrays
+
+console.log(array2d); // prints 2d array
+console.table(array2d); // prints 2d arrays in the form of a table
+let res = array2d[1]; // [4, 5, 6]
+console.log(res);
+console.log('res of 2nd index');
+console.log(res[2]);
 console.log(array2d[1][2]);
