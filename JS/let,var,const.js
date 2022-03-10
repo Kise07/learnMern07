99cc6f58d2b7b898ad69f6e1dd3971ca81b58454
kise` <kise@kises-MacBook-Air.local>
2022-03-10 20:04:15 +0530

    closures started

--- JS/let,var,const.js
+++ JS/let,var,const.js
@@ -33,13 +33,13 @@
 // console.log(a);
 // console.log(b);
 
-let b = 10;
-{
-	var a = 10;
-	let b = 20;
-	const c = 30;
-	console.log(a);
-	console.log(b);
-	console.log(c);
-}
-console.log(b);
+// let b = 10;
+// {
+// 	var a = 10;
+// 	let b = 20;
+// 	const c = 30;
+// 	console.log(a);
+// 	console.log(b);
+// 	console.log(c);
+// }
+// console.log(b);
