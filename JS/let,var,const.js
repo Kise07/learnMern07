fcc77c757f570bceec38bbe57e576b8b4e975813
kise` <kise@kises-MacBook-Air.local>
2022-03-10 19:53:00 +0530

    blockscope,shadowing

--- JS/let,var,const.js
+++ JS/let,var,const.js
@@ -1,4 +1,48 @@
 console.log(x);
 var x = 10;
 let b = 100;
+// console.log(x);
+// var x = 10;
+// let b = 100;
+// console.log(b);
+
+// --block
+// -> it used for compound statements
+// {
+
+// }
+
+// if (10>0){
+//     // do something
+//     // 2nd state
+//     // 3rd statement
+// }
+
+// block scope -> variables declared in a block are accessible inside that block, those variables are known to be block scoped.
+// --varibles declared using let and const are block scoped
+// var is function scoped, it is not block scoped
+// var a = 100;
+
+// {
+// 	var a = 10;
+// 	let b = 20;
+// 	const c = 30;
+
+// 	console.log(a);
+// 	console.log(b);
+// 	console.log(c);
+// }
+
+// console.log(a);
+// console.log(b);
+
+let b = 10;
+{
+	var a = 10;
+	let b = 20;
+	const c = 30;
+	console.log(a);
+	console.log(b);
+	console.log(c);
+}
 console.log(b);
