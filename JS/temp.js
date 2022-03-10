085106cd7b57ca1c5d898ff5e567d5cd9aee8d06
kise` <kise@kises-MacBook-Air.local>
2022-03-10 21:16:49 +0530

    nodejs started

---
+++ JS/temp.js
@@ -0,0 +1,9 @@
+var obj = {
+	add: function add(a, b) {
+		return a + b;
+	},
+
+	sub: function subtract(a, b) {
+		return b - a;
+	},
+};
---
+++ Node/fs.js
@@ -0,0 +1,17 @@
+// fs -> file system module -> it helps us to make files/folders append data in them, delete data, read data
+const fs = require('fs'); // require('path_of_the_file);
+// cosnt f1=require('./f1.txt');
+const abc = require('../JS/temp');
+console.log(abc);
+
+// console.log(fs);
+// appendFileSync appends data into a file, if file is not present, it create the file and then appends the data
+let res = fs.appendFileSync('f1.txt', 'Hello i am f2 file');
+fs.appendFileSync('f1.txt', '\nYou guys are smart');
+// console.log(res);
+
+let data = fs.readFileSync('f1.txt');
+console.log(data + '');
+
+// let data = fs.readFileSync('f1.txt', 'utf-8');
+// console.log(data);
