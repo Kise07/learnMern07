16ba9d281e56ea7fdb981f5b2fba9dff997f8188
kise <kaiazmi1412@gmail.com>
2022-04-15 20:06:26 +0530

    algo page is opened

--- Node/automation/hackerrank/hackerRank.js
+++ Node/automation/hackerrank/hackerRank.js
@@ -1,9 +1,9 @@
 const puppeteer = require("puppeteer");
-// let {email} =
-let email = "";
-let password = "";
+let { email, password } = require("./secrets");
+// let email = "";
+// let password = "";
 
-let cTab;
+let curTab;
 let browserOPenPromise = puppeteer.launch({
 	headless: false,
 	defaultViewportnull: null,
@@ -15,27 +15,31 @@ let browserOPenPromise = puppeteer.launch({
 browserOPenPromise // fulfill
 	.then(function (browser) {
 		console.log("browser is open");
-		console.log(browser);
+		// console.log(browser);
+		// An array of all open pages inside the Browser.
+		// returns an array with all the pages in all browser contexts
 		let allTabsPromise = browser.pages();
 		return allTabsPromise;
 	})
 	.then(function (allTabsArr) {
-		cTab = allTabsArr[0];
+		curTab = allTabsArr[0];
 		console.log("new tab");
 		// URL to navigate page to
-		let visitLoginPagePromise = cTab.goto(
+		let visitLoginPagePromise = curTab.goto(
 			"https://www.hackerrank.com/auth/login"
 		);
 		return visitLoginPagePromise;
 	})
-	.then(function () {
+	.then(function (data) {
+		// console.log(data);
 		console.log("Hackerrank login page opened");
-		let emailWillBeTypedPromise = cTab.type("input[name='username']", email);
+		//selector(where to type), data(what to type)
+		let emailWillBeTypedPromise = curTab.type("input[name='username']", email);
 		return emailWillBeTypedPromise;
 	})
 	.then(function () {
 		console.log("email is typed");
-		let passwordWillBeTypedPromise = cTab.type(
+		let passwordWillBeTypedPromise = curTab.type(
 			"input[type='password']",
 			password
 		);
@@ -43,14 +47,46 @@ browserOPenPromise // fulfill
 	})
 	.then(function () {
 		console.log("password has been typed");
-		let willBeLoggedInPromise = cTab.click(
+		let willBeLoggedInPromise = curTab.click(
 			".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled"
 		);
 		return willBeLoggedInPromise;
 	})
 	.then(function () {
 		console.log("logged into hackerrank successfully");
+		// waitAndClick will wait for the selector to load , and then click on the node
+		let algorithmTabWillBeOPenedPromise = waitAndClick(
+			"div[data-automation='algorithms']"
+		);
+		return algorithmTabWillBeOPenedPromise;
+	})
+	.then(function () {
+		console.log("algorithm pages is opened");
 	})
 	.catch(function (err) {
 		console.log(err);
 	});
+
+function waitAndClick(algoBtn) {
+	let waitClickPromise = new Promise(function (resolve, reject) {
+		let waitForSelectorPromise = curTab.waitForSelector(algoBtn);
+		waitForSelectorPromise
+			.then(function () {
+				console.log("algo btn is found");
+				let clickPromise = curTab.click(algoBtn);
+				return clickPromise;
+			})
+			.then(function () {
+				console.log("algo btn is clicked");
+				// resolve();
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+	});
+
+	// waitClickPromise.then(function () {
+	// 	console.log("inside .then of waitClickPromise");
+	// });
+	return waitClickPromise;
+}
